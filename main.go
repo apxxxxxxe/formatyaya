b728b82a1761a1e876dd00aeba3e44e82bd23da6
apxxxxxxe <calcium629@gmail.com>
2022-11-26 16:05:05 +0900

    update: Asignほか演算子を実装

--- main.go
+++ main.go
@@ -24,47 +24,102 @@ type Func struct {
 }
 
 type FuncEntity struct {
-	Expr *Expr `( @@`
+	Main *Expr `( @@`
 	Sub  *Expr `| "{" "\n"* (@@ "\n"*)* "}")`
 }
 
+// 関数内に1行で取りうる式
 type Expr struct {
-	Left  *Terminal `@@`
-	Op    string    `( @Oper`
-	Right *Terminal `  @@    )?`
+	Asign     *Asign `( @@ `
+	Separator string `| @Separator)`
 }
 
-type Terminal struct {
-	String *String `( @@`
-	Ident  string  `| @Ident)`
+// 代入式
+type Asign struct {
+	Left *Primary `@@`
 
-	Call *Call `("(" @@ ")")?`
+	OperAsign      string      `( @OperAsign`
+	Right          *Comparison `  @@        `
+	OperAsignUnary string      `| @OperAsignUnary)?`
 }
 
-type Call struct {
-	Terminal []*Expr `(@@ ","?)*`
+type MuitiPulation struct {
+	Left *Addition `@@`
+
+	Op    string         `( @("*"|"/"|"%")`
+	Right *MuitiPulation `  @@)*`
+}
+
+type Addition struct {
+	Left *Comparison `@@`
+
+	Op    string    `( @("+"|"-")`
+	Right *Addition `  @@)*`
+}
+
+type Comparison struct {
+	Left *Logic `@@`
+
+	OperComp string      `( @OperComp`
+	Right    *Comparison `  @@)*`
+}
+
+type Logic struct {
+	Left *Primary `@@`
+
+	AND struct{} `(( "&&"`
+	OR  struct{} ` | "||")`
+
+	Right *Logic `@@)*`
+}
+
+// 単項: 左辺と右辺の両方になりうる式
+type Primary struct {
+	String   *String   `( @@`
+	FuncCall *FuncCall `| @@`
+	Number   *Number   `| @@`
+	Ident    string    `| @Ident)`
+}
+
+type Flow struct {
+	Key string `@Flow`
+}
+
+type FuncCall struct {
+	FuncName string     `@Ident`
+	Args     []*Primary `"(" (@@ ","?)* ")"`
 }
 
 type String struct {
 	Value string `"\"" @Char "\""`
 }
 
+type Number struct {
+	Value float64 `@Number`
+}
+
 var (
 	def = lexer.MustStateful(lexer.Rules{
 		"Root": { // Rootは特別な名前　これが基点のRule
 			// TODO: defineとか
-			{Name: "Ident", Pattern: `\w+`, Action: nil},
 			{Name: `Function`, Pattern: `\{`, Action: lexer.Push("FuncRule")},
 			lexer.Include("Common"),
 		},
 		"FuncRule": {
-			{Name: "Ident", Pattern: `\w+`, Action: nil},
-			{Name: "String", Pattern: `"`, Action: lexer.Push("StringRule")},
-			{Name: `Oper`, Pattern: `(-|\+|/|\*|%|=|==|!=|>=|<=|>|<)`, Action: nil},
-			{Name: "Call", Pattern: `\(`, Action: lexer.Push("CallRule")},
+			{Name: `OperAsignUnary`, Pattern: `(--|\+\+)`, Action: nil},
+			{Name: `OperUnary`, Pattern: `!`, Action: nil}, // TODO: -はない？要確認
+			{Name: `OperCalc`, Pattern: `(-|\+|/|\*|%)`, Action: nil},
+			{Name: `OperLogic`, Pattern: `(\|\||&&)`, Action: nil},
+			{Name: `OperComp`, Pattern: `(==|!=|>=|<=|>|<|_in_|!_in_)`, Action: nil},
+			{Name: `OperAsign`, Pattern: `(=|:=|\+=|-=|\*=|/=|%=|\+:=|-:=|\*:=|/:=|%:=|,=)`, Action: nil},
+			{Name: "FlowExpr", Pattern: `(while|if|elseif)`, Action: nil},
 			{Name: `Function`, Pattern: `\{`, Action: lexer.Push("FuncRule")},
-			{Name: "FuncEnd", Pattern: `\}`, Action: lexer.Pop()},
+			{Name: `Separator`, Pattern: `--`, Action: nil},
+			{Name: "Number", Pattern: `\d+(\.\d+)?`, Action: nil},
+			{Name: "String", Pattern: `"`, Action: lexer.Push("StringRule")},
 			lexer.Include("Common"),
+			{Name: "Call", Pattern: `\(`, Action: lexer.Push("CallRule")},
+			{Name: "FuncEnd", Pattern: `\}`, Action: lexer.Pop()},
 		},
 		"StringRule": {
 			{Name: "Char", Pattern: `[^"]+`, Action: nil},
@@ -78,6 +133,7 @@ var (
 		"Common": {
 			{Name: "LF", Pattern: `\n`, Action: nil},
 			{Name: `Whitespace`, Pattern: `\s+`, Action: nil},
+			{Name: "Ident", Pattern: `\w[^ \n!"#$%&()*+,-/:;<=>?@\[\]{|}]*`, Action: nil}, //TODO:`も禁止文字に入れる
 		},
 	})
 	parser = participle.MustBuild[Root](
@@ -88,13 +144,23 @@ var (
 
 func main() {
 	src := `
-  Func
+  FuncHoge
   {
     {
       e = f(g,func("h"))
     }
     a = b(c,"d")
   }
+
+  FuncFuga
+  {
+    {
+      e = f(g,func("h"))
+    }
+    a = b(c,"d") || vim == 1
+    p++
+  }
+
   `
 
 	actual, err := parser.Parse("", strings.NewReader(src))
