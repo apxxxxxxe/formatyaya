b4b74d1ed17cd5e82e6b9be72b4ee6fabcff6977
apxxxxxxe <calcium629@gmail.com>
2022-12-10 07:39:03 +0900

    fix: 空行のタブ文字・半角スペースを許容

--- lexer.go
+++ lexer.go
@@ -10,7 +10,7 @@ type TokenDict = map[lexer.TokenType]string
 var (
 	def = lexer.MustStateful(lexer.Rules{
 		"Root": { // Rootは特別な名前　これが基点のRule
-			{Name: "BlankLine", Pattern: `\n\n`, Action: nil},
+			{Name: "BlankLine", Pattern: `\n[\t ]*\n`, Action: nil},
 			{Name: "LF", Pattern: `\n`, Action: nil},
 			{Name: `Space`, Pattern: ` +`, Action: nil},
 			{Name: `TabSpace`, Pattern: `	+`, Action: nil},
@@ -33,7 +33,7 @@ var (
 			{Name: `Space`, Pattern: ` +`, Action: nil},
 			{Name: `TabSpace`, Pattern: `	+`, Action: nil},
 			{Name: `ExprEnd`, Pattern: `;`, Action: nil},
-			{Name: "BlankLine", Pattern: `\n\n`, Action: nil},
+			{Name: "BlankLine", Pattern: `\n[\t ]*\n`, Action: nil},
 			{Name: "LF", Pattern: `\n`, Action: nil},
 			lexer.Include("Comments"),
 			{Name: "SingleQuoteString", Pattern: `'`, Action: lexer.Push("SingleQuoteStringRule")},
