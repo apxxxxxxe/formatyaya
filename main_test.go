0a1884ac04df4754bfc2b10d482bc0905944c465
apxxxxxxe <calcium629@gmail.com>
2022-12-10 19:56:19 +0900

    update: test

--- main_test.go
+++ main_test.go
@@ -1,35 +1,55 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 )
 
-const dirname = "testfiles"
+const dirname = "ghost"
+const destDir = "out"
+
+func findFilesWithWalkDir(root, ext string) ([]string, error) {
+	findList := []string{}
+
+	err := filepath.WalkDir(root, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if !strings.HasSuffix(path, ext) {
+			return nil
+		}
+
+		findList = append(findList, path)
+		return nil
+	})
+	return findList, err
+}
 
 func TestMain(*testing.T) {
-	finfo, err := ioutil.ReadDir(dirname)
+	files, err := findFilesWithWalkDir(dirname, "dic")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, f := range finfo {
-		// if i >= 2 {
-		// 	break
-		// }
+	fmt.Println(files)
 
-		if strings.HasPrefix(f.Name(), "replaced_") {
-			continue
-		}
+	for _, src := range files {
+		dest := strings.Replace(src, dirname, destDir, 1)
 
-		if err := os.MkdirAll("out", 0755); err != nil {
+		if err := os.MkdirAll(filepath.Dir(dest), 0755); err != nil {
 			panic(err)
 		}
 
-		if err := os.WriteFile(filepath.Join("out", "out_"+f.Name()), []byte(parse(filepath.Join(dirname, f.Name())).String()), 0644); err != nil {
+		if err := os.WriteFile(dest, []byte(parse(src).String()), 0644); err != nil {
 			panic(err)
 		}
 	}
